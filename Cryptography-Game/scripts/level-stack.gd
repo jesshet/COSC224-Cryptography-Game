c89353d2de006b2e8f71cd405ced5c784149a317
Swambe <sammichaelblair@gmail.com>
2025-03-06 09:36:34 -0800

    Cleaned up scene tree organization and altered leve shader colours

--- Cryptography-Game/scripts/level-stack.gd
+++ Cryptography-Game/scripts/level-stack.gd
@@ -4,7 +4,7 @@ var Solution;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Solution = "[center]ATTACK AT DAWN"
+	Solution = "[center]ATTACK AT DAWN";
 	connect_caesar();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
