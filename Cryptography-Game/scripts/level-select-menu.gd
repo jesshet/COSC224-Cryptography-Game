9b16417b5ff41a16fa18e0730f5c8481f4eeacde
Brandon0C <brandon.mills@myokanagan.bc.ca>
2025-03-11 00:45:50 -0700

    level select menu added

---
+++ Cryptography-Game/scripts/level-select-menu.gd
@@ -0,0 +1,24 @@
+extends Container
+
+var _root: Node;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_root = get_tree().get_root();
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_level_load_level(level: PackedScene) -> void:
+	if level == null:
+		print("Level was null");
+		return;
+	print(level.resource_path);
+	var newLevel = level.instantiate();
+	_root.add_child(newLevel);
+	queue_free();
+
+func _on_close_button_pressed() -> void:
+	queue_free()
