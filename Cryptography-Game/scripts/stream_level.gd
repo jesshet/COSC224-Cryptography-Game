ea9368a6b0d2f34aabd995765d259b9a58b61982
Swambe <sammichaelblair@gmail.com>
2025-03-27 10:34:16 -0700

    Added submit boxes to stream level and XOR box

---
+++ Cryptography-Game/scripts/stream_level.gd
@@ -0,0 +1,17 @@
+extends Control
+var solution
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	solution = "hello"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+func _on_submitbox_submit() -> void:
+	var answer = $"submit-box/TextEdit".text
+	if(answer == solution):
+		print("you won")
