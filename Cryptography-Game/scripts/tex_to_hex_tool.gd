0232aeace7f0f19b222d39d92ea2b6fb614ae604
Jesse H <hetjess@gmail.com>
2025-04-01 12:28:54 -0700

    committing

--- Cryptography-Game/scripts/tex_to_hex_tool.gd
+++ Cryptography-Game/scripts/tex_to_hex_tool.gd
@@ -8,6 +8,7 @@ var infoSet
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#Setting the regex that will match hexadecimel strings
+
 	infoSet = false
 	textBox = $CollisionContainer/Area2D
 	_inputText = ""
@@ -35,9 +36,34 @@ func _on_text_to_hex_btn_pressed() -> void:
 		return
 		
 	var hex = ""
-	var bytes = _inputText.to_utf8_buffer()
-	for byte in bytes:
-		hex += String("%02x" % byte)
+	#redo:
+	var i = 0
+	while i < _inputText.length():
+		print("Loop iteration start. i = ")
+		print(i)#switch to 32 offset and use tilde as escape char
+		print("Looking at input char: " + _inputText[i])
+		if _inputText[i] == "~":
+			var byte1 = _inputText.substr(i+1, 1).to_utf8_buffer()[0] - 32
+			print("Half Byte 1: %x" % byte1)
+			var byte2 = _inputText.substr(i+2, 1).to_utf8_buffer()[0] - 32
+			print("Half Byte 2: %x" % byte1)
+			hex += String("%x" % byte1)
+			hex += String("%x" % byte2)
+			i += 3
+		else:
+			var byte = _inputText.substr(i, 1).to_utf8_buffer()[0] - 32
+			print(_inputText.substr(i, 1) + "becomes big byte: %02x" % byte)
+			hex += String("%02x" % byte)
+			#print(_inputText.substr(i, 2) - 34)
+			i += 1
+		print("i is now ")
+		print(i)
+	print("End string: " + hex)
+	
+	#var bytes = _inputText.to_utf8_buffer()
+	#print(bytes)
+	#for byte in bytes:
+	#	hex += String("%02x" % byte)
 	_inputText = hex.replace(" ","").to_upper()
 	#textBox.node.text = _inputText
 	animateChange(textBox.node.text, _inputText)
@@ -58,12 +84,12 @@ func _on_hex_to_text_btn_pressed() -> void:
 	for i in range(0, _inputText.length(), 2):
 		var byte_hex = _inputText.substr(i, 2)
 		byte_hex = byte_hex #make sure it starts at '!'
-		if byte_hex.hex_to_int() + 33 > 133 :
-			var b1 = byte_hex.substr(0,1).hex_to_int() + 33
-			var b2 = byte_hex.substr(1,2).hex_to_int() + 33
-			display_plaintext = display_plaintext + char(b1) + char(b2)
+		if byte_hex.hex_to_int() + 34 > 133 :
+			var b1 = byte_hex.substr(0,1).hex_to_int() + 32
+			var b2 = byte_hex.substr(1,2).hex_to_int() + 32
+			display_plaintext = display_plaintext + "~" + char(b1) + char(b2)
 		else:
-			display_plaintext += char(byte_hex.hex_to_int() + 33)
+			display_plaintext += char(byte_hex.hex_to_int() + 32)
 		#bytes.append(byte_hex)
 	#_inputText = bytes.get_string_from_utf8()
 	print(display_plaintext)
